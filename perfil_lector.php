f98393a315db649422006e2ebae4c0d0d9da8911
floreacosta <florem.10@hotmail.com>
2016-06-27 17:18:12 -0300

    Add files via upload

---
+++ perfil_lector.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"/>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <?php
+		include_once('include/head.php');
+		include_once('clases/Publicacion.php');
+	?>
+	<body>
+		<?php include_once('include/header_body.php'); ?>
+		
+		<section class='perfil content'>
+			<div class='ubicacion'>
+				<p><a href='index.php'>Home</a> <span class='separacion'>></span> <a class='here' href='perfil_lector.php'>Perfil de usuario</a></p>
+			</div>
+			
+			<div class='perfil'>
+				<img class='fotoPerfil' src='#'/>
+				<div class='datosPerfil'>
+					<h2>Bienvenido/a</h2>
+					<h1>Carlos Alberto <a class='iconModificar' href='perfil_modif.php'>r</a></h1>
+				</div>
+			</div>
+			
+			<div class='comprasUsuario'>
+				<h2 class='tituloCompras'>Mis compras</h2>
+				<figure class='col'><!-- Item Compra 1 -->
+					<p class='numCompra'>N°: 001</p>
+					<div class='informacion'>
+						<img class='tapaRevista' src='img/thumbs-publicacion/revista1.png'/>
+						<div class='informacionEdicion'>
+							<p class='fechaEdicion'>10/10/2015</p>
+							<h3>Nombre de la edición</h3>
+						</div>
+					</div>
+					<p class='fechaCompra'>Fecha de compra: 10/15/2016</p>
+					<p class='costoCompra'>Precio: $45.00</p>
+				</figure>
+				<figure class='col'><!-- Item Compra 2 -->
+					<p class='numCompra'>N°: 001</p>
+					<div class='informacion'>
+						<img class='tapaRevista' src='img/thumbs-publicacion/revista1.png'/>
+						<div class='informacionEdicion'>
+							<p class='fechaEdicion'>10/10/2015</p>
+							<h3>Nombre de la edición</h3>
+						</div>
+					</div>
+					<p class='fechaCompra'>Fecha de compra: 10/15/2016</p>
+					<p class='costoCompra'>Precio: $45.00</p>
+				</figure>
+				<figure class='col'><!-- Item Compra 3 -->
+					<p class='numCompra'>N°: 001</p>
+					<div class='informacion'>
+						<img class='tapaRevista' src='img/thumbs-publicacion/revista1.png'/>
+						<div class='informacionEdicion'>
+							<p class='fechaEdicion'>10/10/2015</p>
+							<h3>Nombre de la edición</h3>
+						</div>
+					</div>
+					<p class='fechaCompra'>Fecha de compra: 10/15/2016</p>
+					<p class='costoCompra'>Precio: $45.00</p>
+				</figure>
+				<figure class='col'><!-- Item Compra 4 -->
+					<p class='numCompra'>N°: 001</p>
+					<div class='informacion'>
+						<img class='tapaRevista' src='img/thumbs-publicacion/revista1.png'/>
+						<div class='informacionEdicion'>
+							<p class='fechaEdicion'>10/10/2015</p>
+							<h3>Nombre de la edición</h3>
+						</div>
+					</div>
+					<p class='fechaCompra'>Fecha de compra: 10/15/2016</p>
+					<p class='costoCompra'>Precio: $45.00</p>
+				</figure>
+				<figure class='col'><!-- Item Compra 5 -->
+					<p class='numCompra'>N°: 001</p>
+					<div class='informacion'>
+						<img class='tapaRevista' src='img/thumbs-publicacion/revista1.png'/>
+						<div class='informacionEdicion'>
+							<p class='fechaEdicion'>10/10/2015</p>
+							<h3>Nombre de la edición</h3>
+						</div>
+					</div>
+					<p class='fechaCompra'>Fecha de compra: 10/15/2016</p>
+					<p class='costoCompra'>Precio: $45.00</p>
+				</figure>
+			</div>
+			
+			<div class='suscripcionUsuario'>
+				<h2 class='tituloSuscripcion'>Mis suscripciones</h2>
+				<figure class='col'><!-- Item Compra 1 -->
+					<p class='numCompra'>N°: 001</p>
+					<div class='informacion'>
+						<div class='informacionEdicion'>
+							<h3>Nombre de la Revista</h3>
+						</div>
+					</div>
+					<p class='fechaCompra'>Fecha de inicio: 10/15/2016</p>
+					<p class='fechaCompra'>Fecha de fin: 10/15/2016</p>
+					<p class='costoSuscripcion'>Precio: $45.00</p>
+					<a class='cancelarSuscripcion' href='#'>Cancelar suscripción</a>
+				</figure>
+				<figure class='col'><!-- Item Compra 2 -->
+					<p class='numCompra'>N°: 001</p>
+					<div class='informacion'>
+						<div class='informacionEdicion'>
+							<h3>Nombre de la Revista</h3>
+						</div>
+					</div>
+					<p class='fechaCompra'>Fecha de inicio: 10/15/2016</p>
+					<p class='fechaCompra'>Fecha de fin: 10/15/2016</p>
+					<p class='costoSuscripcion'>Precio: $45.00</p>
+					<a class='cancelarSuscripcion' href='#'>Cancelar suscripción</a>
+				</figure>
+				<figure class='col'><!-- Item Compra 3 -->
+					<p class='numCompra'>N°: 001</p>
+					<div class='informacion'>
+						<div class='informacionEdicion'>
+							<h3>Nombre de la Revista</h3>
+						</div>
+					</div>
+					<p class='fechaCompra'>Fecha de inicio: 10/15/2016</p>
+					<p class='fechaCompra'>Fecha de fin: 10/15/2016</p>
+					<p class='costoSuscripcion'>Precio: $45.00</p>
+					<a class='cancelarSuscripcion' href='#'>Cancelar suscripción</a>
+				</figure>
+				<figure class='col'><!-- Item Compra 4 -->
+					<p class='numCompra'>N°: 001</p>
+					<div class='informacion'>
+						<div class='informacionEdicion'>
+							<h3>Nombre de la Revista</h3>
+						</div>
+					</div>
+					<p class='fechaCompra'>Fecha de inicio: 10/15/2016</p>
+					<p class='fechaCompra'>Fecha de fin: 10/15/2016</p>
+					<p class='costoSuscripcion'>Precio: $45.00</p>
+					<a class='cancelarSuscripcion' href='#'>Cancelar suscripción</a>
+				</figure>
+				<figure class='col'><!-- Item Compra 5 -->
+					<p class='numCompra'>N°: 001</p>
+					<div class='informacion'>
+						<div class='informacionEdicion'>
+							<h3>Nombre de la Revista</h3>
+						</div>
+					</div>
+					<p class='fechaCompra'>Fecha de inicio: 10/15/2016</p>
+					<p class='fechaCompra'>Fecha de fin: 10/15/2016</p>
+					<p class='costoSuscripcion'>Precio: $45.00</p>
+					<a class='cancelarSuscripcion' href='#'>Cancelar suscripción</a>
+				</figure>
+
+			</div>
+		</section>
+		
+		<?php include_once('include/footer.php'); ?>
+	</body>
+</html>
---
+++ perfil_modificacion.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"/>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <?php
+		include_once('include/head.php');
+		include_once('clases/Publicacion.php');
+	?>
+	<body>
+		<?php include_once('include/header_body.php'); ?>
+		
+		<section class='modificacionDatos content'>
+			<div class='ubicacion'>
+				<p><a href='index.php'>Home</a><span class='separacion'>></span><a class='here' href='perfil_lector.php'>Perfil de usuario</a><span class='separacion'>></span><a class='here' href='perfil_modificacion.php'>Modificacion/Alta de usuario</a></p>
+			</div>
+			<div class='formularioUsuario'>
+				<h2>Modifique o cree un usuario</h2>
+				<form action='perfil_usuario.php' method='POST' enctype=''>
+					<label for='nombreUsuario'>Nombre</label>
+					<input type='text' name='nombreUsuario' placeholder='Carlos' value=''/>
+
+					<label for='nombreUsuario'>Apellido</label>
+					<input type='text' name='apellidoUsuario' placeholder='Menendez' value=''/>
+
+					<label for='dniUsuario'>Dni</label>
+					<input type='text' name='dniUsuario' placeholder='9 dígitos' value=''/>
+
+					<label for='paisUsuario'>País</label>
+					<select name='paisUsuario'>
+						<option value='' selected>Seleccionar país</option>
+						<option value='1'>Argentina</option>
+						<option value='2'>Brasil</option>
+					<select/>
+
+					<label for='provinciaUsuario'>Provincia</label>
+					<select name='provinciaUsuario'>
+						<option value='' selected>Seleccionar provincia</option>
+						<option value='1'>Buenos Aires</option>
+						<option value='2'>Entre Ríos</option>
+					<select/>
+
+					<label for='localidadUsuario'>Localidad</label>
+					<select name='localidadUsuario'>
+						<option value='' selected>Seleccionar localidad</option>
+						<option value='1'>Hurlingham</option>
+						<option value='2'>Morón</option>
+					<select/>
+
+					<label for='passUsuario'>Contraseña</label>
+					<input type='password' name='passUsuario' placeholder='contraseña' value=''/>
+
+					<label for='passRepetirUsuario'>Repetir Contraseña</label>
+					<input type='password' name='passRepetirUsuario' placeholder='contraseña' value=''/>
+					
+					<input type='submit' name='enviarUsuario' value='Enviar'/>
+				</form>
+			</div>
+		</section>
+		
+		<?php include_once('include/footer.php'); ?>
+	</body>
+</html>
