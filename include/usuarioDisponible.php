f6a2690bad8ac874968f4cb7ff354b551c042620
Ro14 <specialneeds_Ro@hotmail.com>
2016-06-28 12:49:57 -0300

    Add files via upload
    Buscador de usuario para registro

---
+++ include/usuarioDisponible.php
@@ -0,0 +1,16 @@
+<?php
+	include_once('../clases/BaseDatos.php');
+	$usuario = $_GET["usuario"];
+	$bd= new BaseDatos('localhost', 'root', '', 'dbredaccion');
+	$sql = "
+	SELECT *
+	FROM usuarioLector";
+	$resultado = mysqli_query($bd->getEnlace(), $sql);
+	while($fila = mysqli_fetch_assoc($resultado)){
+		if($fila["usuario"]==$usuario){
+			echo"El nombre de usuario ".$usuario." no esta disponibles";
+		}
+		else{
+		}
+	}
+?>
