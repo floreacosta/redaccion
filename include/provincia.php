940f272d6650076bb361ba986d7fe463a1f0a903
Ro14 <specialneeds_Ro@hotmail.com>
2016-06-28 00:28:31 -0300

    Add files via upload

---
+++ include/provincia.php
@@ -0,0 +1,14 @@
+<?php
+include_once('../clases/BaseDatos.php');
+$pais = $_GET["pais"];
+$bd= new BaseDatos('localhost', 'root', '', 'dbredaccion');
+$sql = "
+SELECT *
+FROM provincia 
+WHERE idPais = " . $pais;
+$resultado = mysqli_query($bd->getEnlace(), $sql);
+echo "<option value=''>Seleccionar</option>";
+while($fila = mysqli_fetch_assoc($resultado)){
+    echo "<option value='" . $fila["idProvincia"] . "'>" . $fila["nombre"] . "</option>";
+}
+?>
