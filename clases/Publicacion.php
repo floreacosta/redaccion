80b125eb748a82938f890276a1052f9388f6d1fa
Fernando Matias Muñoz <fernandomatiasmunoz@hotmail.com>
2016-06-29 01:14:37 -0300

    cambiar las publicaciones listadas

--- clases/Publicacion.php
+++ clases/Publicacion.php
@@ -3,11 +3,12 @@
 	class Publicacion{
 		
 		public function mostrarPublicaciones($topeId){
+			
 			$bd = new BaseDatos('localhost', 'root', '', 'dbredaccion');
 			$strSql = "
 				SELECT ED.idEdicion, ED.tituloEdicion, Ed.imagenTapaEdicion, ED.fecha, ED.precio
 				FROM edicion ED
-				ORDER BY ED.idEdicion LIMIT $topeId, 10
+				ORDER BY ED.idEdicion DESC LIMIT $topeId, 10
 			";
 
 			$consulta = mysqli_query($bd->getEnlace(), $strSql);
@@ -32,17 +33,67 @@
 						<div class='infoPublicacion'>
 							<div class='precioPublicacion'>$".$_SESSION['precioEdicion'].".00</div>
 							<div class='comprarPublicacion'>
-								<a class='comprar' href='comprar.php?idEdicion=".$_SESSION['idEdicion']."'>Comprar</a>
+								<a class='comprar' href='comprar.php?edicion=".$_SESSION['idEdicion']."'>Comprar</a>
 							</div>
 						</div>
 					</figcaption>
 				</figure>
 				";
 			}
+			echo "<div class='cargarMas noFlote'>";
+			if ($topeId > 0){
+				echo "<a  href=\"index.php?desde=".($topeId - 10)."\" >Anterior</a> ";
+			}
+			if ($topeId > 0 && mysqli_num_rows( $consulta ) == 10){
+				echo "<h7>| </h7>";
+			}
+			if (mysqli_num_rows( $consulta ) == 10){
+				
+				echo "<a href=\"index.php?desde=".($topeId + 10)."\" >Siguiente</a>";
+			}
+			echo "</div>";
+			
 		}
-		
-		public function publicacionRandom(){
-			//traer una publicacion random para Index.
+		public function edicionRandom(){
+			$bd = new BaseDatos('localhost', 'root', '', 'dbredaccion');
+			
+			$strSql = "
+				SELECT ED.idEdicion, ED.tituloEdicion, Ed.imagenTapaEdicion, ED.fecha, ED.precio
+				FROM edicion ED
+				ORDER BY rand() LIMIT 1
+			";
+
+			$consulta = mysqli_query($bd->getEnlace(), $strSql);
+			
+			$encontro = FALSE;
+			if($edicion = mysqli_fetch_assoc($consulta)){
+				$idEdicion = $edicion['idEdicion'];
+				$tituloEdicion = $edicion['tituloEdicion'];
+				$imagenTapaEdicion = $edicion['imagenTapaEdicion'];
+				$fechaEdicion = $edicion['fecha'];
+				$precioEdicon = $edicion['precio'];
+						
+				echo "
+				<figure class='columna'>
+					<div class='imgPublicacion'>
+						<img src='".$imagenTapaEdicion."'/>
+					</div>
+				</figure>
+				<figcaption class='columna'>
+					<div>
+						<h1>".$tituloEdicion."</h1>
+						<h5>Publicada: ".$fechaEdicion."</h5>
+						<p>Tres ilustradores: Irma Gruenhalz / Car Pintos / Chris Buzeli.</p>
+					</div>
+					<div class='infoPublicacion'>
+						<div class='precioPublicacion'>$".$precioEdicon.".00</div>
+						<div class='comprarPublicacion'>
+							<button class='comprar' value='comprar' onClick=\"window.location.href='comprar.php?edicion=".$idEdicion."';\" id='comprar'>Comprar</button>
+						</div>
+					</div>
+				</figcaption>";
+				//cargar edicon Random
+			}
 		}
 		
 		public function listarPublicacion($topeId, $tipo){
@@ -128,7 +179,7 @@
 			//boton "agregar"
 		}
 		
-		public function buscarPublicacion($topeId){
+		public function buscarEdicion($topeId){
 			$bd = new BaseDatos('localhost', 'root', '', 'dbredaccion');
 			
 			$strSql = "
@@ -161,7 +212,7 @@
 						<div class='infoPublicacion'>
 							<div class='precioPublicacion'>$".$_SESSION['precioEdicion'].".00</div>
 							<div class='comprarPublicacion'>
-								<a class='comprar' href='comprar.php?idEdicion=".$_SESSION['idEdicion']."'>Comprar</a>
+								<a class='comprar' href='comprar.php?edicion=".$_SESSION['idEdicion']."'>Comprar</a>
 							</div>
 						</div>
 					</figcaption>
