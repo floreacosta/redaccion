aae46ae558e230fe477f1396c7f0cf3ef93d24ea
Fernando Matias Muñoz <fernandomatiasmunoz@hotmail.com>
2016-06-29 01:14:16 -0300

    cambiar las publicaciones listadas
    ir cambiando las publicaciones de a diez

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <?php
+		
 		include_once('include/head.php');
 		include_once('clases/Publicacion.php');
 	?>
@@ -29,24 +30,10 @@
 			<div class='publicacionRandom'>
 				<div class='contenedor'>
 					<div class='random contenido'>
-						<figure class='columna'>
-							<div class='imgPublicacion'>
-								<img src='img/thumbs-publicacion/revista1.png'/>
-							</div>
-						</figure>
-						<figcaption class='columna'>
-							<div>
-								<h1>90 + 10</h1>
-								<h5>Publicada: 10/05/2016</h5>
-								<p>Tres ilustradores: Irma Gruenhalz / Car Pintos / Chris Buzeli.</p>
-							</div>
-							<div class='infoPublicacion'>
-								<div class='precioPublicacion'>$45.00</div>
-								<div class='comprarPublicacion'>
-									<button class='comprar' value='comprar' id='comprar'>Comprar</button>
-								</div>
-							</div>
-						</figcaption>
+						<?php
+							$publicacion = new Publicacion();
+							$publicacion->edicionRandom();
+						?>
 					</div>
 				</div>
 				<div class='clima'></div>
@@ -54,16 +41,19 @@
 		</section>
 
 		<section class='publicacion content'>
-			<h2>Publicaciones disponibles</h2>
+			<a name="publicaciones"></a><h2>Publicaciones disponibles</h2>
 			<div class='contenidoPublicacion'><!--INICIO PUBLICACIONES-->
 				<?php
-					$publicacion = new Publicacion();
-					$publicacion->mostrarPublicaciones(0);
+					if (!ISSET($_GET['desde'])){
+						$desde = 0;
+					}else{
+						$desde = $_GET['desde'];
+					}
+							
+					$publicacion->mostrarPublicaciones($desde);		
 				?>
 			</div><!--FIN PUBLICACIONES-->
-			<div class='cargarMas noFlote'>
-				<a href='#'>Cargar más</a>
-			</div>
+			
 
 		</section>	
 		<?php 
