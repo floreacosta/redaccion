ba8984154ff631185da2a262a02bb55c9fb891d2
floreacosta <florem.10@hotmail.com>
2016-07-10 22:32:40 -0300

    Botón de Mercado Pago

--- include/compras.php
+++ include/compras.php
@@ -1,24 +1,64 @@
 <?php
 	include_once('../clases/BaseDatos.php');
+	require_once('../lib/mercadopago.php');
 
+	$mp = new MP("3811094535663834", "RVMibY1jQ4a15JSMX4xelFXP3kvqjmVx");
+	
 	$idEdicion = $_GET["idEdicion"];
 	$bd= new BaseDatos();
-
+	
 	$sql = "
-			SELECT tituloEdicion, precio
-			FROM edicion
+			SELECT ED.tituloEdicion, ED.precio, ED.imagenTapaEdicion, PU.nombre, TI.descripcion
+			FROM edicion ED
+				JOIN publicacion PU
+					ON ED.idPublicacion = PU.idPublicacion
+				JOIN tipopublicacion TI
+					ON PU.idTipoPublicacion = TI.idTipoPublicacion
 			WHERE idEdicion = ".$idEdicion;
 	$resultado = mysqli_query($bd->getEnlace(), $sql);
 
 	if($fila = mysqli_fetch_assoc($resultado)){
 		$idEdicion = sprintf("%03d", $idEdicion);
+		$precio = $fila['precio'];
+		$imagenTapa = "/img/thumbs-publicacion/".$fila['imagenTapaEdicion'];
+		
+		$preference_data = array(
+			"items" => array(
+				array(
+					"title" => $fila['tituloEdicion'],
+					"currency_id" => "ARS",
+					"picture_url" => $imagenTapa,
+					"description" => $fila['nombre'],
+					"category_id" => $fila['descripcion'],
+					"quantity" => 1,
+					"unit_price" => (int)$precio
+				)
+			)
+		);
+		
+		$preference = $mp->create_preference($preference_data);
+
 		echo"
-			<h2>Compra</h2>
-			<input type='text'  id='numEdicion' name='numEdicion' value='".$idEdicion."'  hidden/>
-			<h3 class='numEdicionCompra'>Edición n°: ".$idEdicion."'</h3>
-			<h3 class='titEdicionCompra'>'".$fila['tituloEdicion']."'</h3>
-			<h3 class='precioCompra'>$".$fila['precio']."</h3>
-			<input type='submit' id='enviarCompra' name='enviarCompra' value='Comprar'></input><!--BOTON COMPRAR -->
+			<form action='index.php' method='POST' enctype='multipart/form-data' class='modalCompra'><!-- CUANDO EXPLIQUEN MERCADO PAGO SE QUITA EL FORM PARA DEJAR MP -->
+				<a href='#' onClick='modalCloseCompra();'>x</a><!-- CUANDO SE BORRE EL FORM, QUEDA ESTE A FUERA -->
+				<div class='in' id='compraArealizar'><!-- CUANDO SE BORRE EL FORM, QUEDA ESTE DIV FUERA -->
+					<h2>Compra</h2>
+					<input type='text id='numEdicion' name='numEdicion' value='".$idEdicion."' hidden/>
+					<h3 class='numEdicionCompra'>Edición n°: ".$idEdicion."'</h3>
+					<h3 class='titEdicionCompra'>'".$fila['tituloEdicion']."'</h3>
+					<h3 class='precioCompra'>$".$fila['precio']."</h3>
+					<input type='text' name='linkMercadoPago' value='".$preference['response']['init_point']."' hidden/>
+					<input type='submit' id='enviarCompra' name='enviarCompra' value='Pagar con Mercado Pago'></input><!--BOTON COMPRAR -->
+				</div>
+			</form>
 		";
 	}
+	
+//<a class='lightblue-M-Ov-ArAll' mp-mode='modal' href='".$preference['response']['init_point']."' name='MPCheckout' onreturn='execute_my_onreturn'>Pagar</a>
+	
+//VENDEDOR: id:220658189; nickname:TT584039; password:qatest8040; email:test_user_90633126@testuser.com
+
+//COMPRADOR: id:220663084; nickname:TETE3346398; password:qatest5173; email:test_user_19027174@testuser.com
 ?>
+
+
--- include/header_body.php
+++ include/header_body.php
@@ -32,6 +32,9 @@
 <div class='login none lectorBox' id='soyLector'>
 	<form action='index.php' method='POST' enctype='multipart/form-data' class='lector'>
 		<a href='#' onClick='modalCloseLector();'>x</a>
+		<ul class='aclaracion'>
+			<li>Lectores</li>
+		</ul>
 		<div class='in'>
 			<input type='text' id='userLector' name='userLector' placeholder='Usuario' value=''></input>
 			<input type='password' id='passLector' name='passLector' placeholder='Contraseña' value=''></input>
@@ -39,7 +42,6 @@
 		</div>
 		<ul class='aclaracion'>
 			<li><a href='perfil_modificacion.php'>Quiero registrarme.</a></li>
-			<li><a href='#'>Olvidé mi usuario y/o contraseña.</a></li>
 		</ul>
 	</form>
 </div>
@@ -47,36 +49,31 @@
 <div class='login none redactorBox' id='soyRedactor' >
 	<form action='index.php' method='POST' enctype='multipart/form-data' class='redactor'>
 		<a href='#' onClick='modalCloseRedactor();'>x</a>
+		<ul class='aclaracion'>
+			<li>Contenidistas</li>
+		</ul>
 		<div class='in'>
 			<input type='text' id='userRedactor' name='userRedactor' placeholder='Usuario' value=''></input>
 			<input type='password' id='passRedactor' name='passRedactor' placeholder='Contraseña' value=''></input>
 			<input type='submit' id='ingresarLoginRedactor' name='ingresarLoginRedactor' value='LogIn'></input><!--BOTON REDACTOR -->
 		</div>
-		<ul class='aclaracion'>
-			<div><a href='#'>Contactar al administrador.</a></div>
-			<div><a href='#'>Olvidé mi usuario y/o contraseña.</a></div>
-		</ul>
 	</form>
 </div>
 
 <div class='login none administradorBox' id='soyAdministrador' >
 	<form action='index.php' method='POST' enctype='multipart/form-data' class='administrador'>
 		<a href='#' onClick='modalCloseAdministrador();'>x</a>
+		<ul class='aclaracion'>
+			<li>Administradores</li>
+		</ul>
 		<div class='in'>
 			<input type='text' id='userAdministrador' name='userAdministrador' placeholder='Usuario' value=''></input>
 			<input type='password' id='passAdministrador' name='passAdministrador' placeholder='Contraseña' value=''></input>
 			<input type='submit' id='ingresarLoginAdministrador' name='ingresarLoginAdministrador' value='LogIn'></input><!--BOTON ADMINISTRADOR -->
 		</div>
-		<ul class='aclaracion'>
-			<div><a href='#'>Olvidé mi usuario y/o contraseña.</a></div>
-		</ul>
 	</form>
 </div>
 
 <div class='login none compraBox' id='compraBox' >
-	<form action='index.php' method='POST' enctype='multipart/form-data' class='modalCompra'><!-- CUANDO EXPLIQUEN MERCADO PAGO SE QUITA EL FORM PARA DEJAR MP -->
-		<a href='#' onClick='modalCloseCompra();'>x</a><!-- CUANDO SE BORRE EL FORM, QUEDA ESTE A FUERA -->
-		<div class='in' id="compraArealizar"><!-- CUANDO SE BORRE EL FORM, QUEDA ESTE DIV FUERA -->
-		</div>
-	</form>
+<!-- Box Comprar edición + botón pagar-->
 </div>
